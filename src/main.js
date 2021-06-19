0abab5f1df1e73b891405c6286cec3b28e2f00aa
nethiwtg <you@example.com>
2021-06-19 16:36:02 +0800

    完成用户列表功能

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL ='https://lianghj.top:8888/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  console.log(config)
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  // config.headers,Authorization=window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http=axios
 Vue.config.productionTip = false
 
