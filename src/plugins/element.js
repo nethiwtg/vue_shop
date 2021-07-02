d321f8f500a74745881f04466d409daac8ec3d5d
nethiwtg <you@example.com>
2021-07-02 19:56:36 +0800

    完成商品功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -7,7 +7,9 @@ import{Button,Form,FormItem,Input,Message,Container,
     Switch,Tooltip,Pagination,
     Dialog,MessageBox,Tag,
     Tree,Select,Option,
-    Cascader,Alert,Tabs,TabPane} from 'element-ui'
+    Cascader,Alert,Tabs,TabPane,
+    Steps,Step,CheckboxGroup,Checkbox,
+    Upload} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -38,6 +40,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Step)
+Vue.use(Steps)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 
 Vue.use(MenuItem)
 
